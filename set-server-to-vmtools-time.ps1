9aa9ec1be62890315304300b9019f3499a12de61
Brian Frost <brian@Birddogsw.local>
2019-11-16 09:45:53 -0600

    it works now - syntax

--- set-server-to-vmtools-time.ps1
+++ set-server-to-vmtools-time.ps1
@@ -3,12 +3,7 @@
 import-module ActiveDirectory
 
 $Computers = Get-ADComputer -Filter {
-    OperatingSystem -Like "Windows Server*"
-    Name -NotLike "EDGE*"
+    OperatingSystem -Like "Windows Server*" -and Name -NotLike "EDGE*"
 } | Select-Object -ExpandProperty Name
 
-Invoke-Command -ComputerName $Computers -ScriptBlock {
-    
-    "C:\Program Files\VMware\VMware Tools\VMwareToolboxCmd.exe",'timesync','enable'
-
-}  | Select-Object ComputerName,Package | Sort-Object -Property ComputerName
+Invoke-Command -ComputerName $Computers -ScriptBlock { "C:\Program Files\VMware\VMware Tools\VMwareToolboxCmd.exe timesync start"}
