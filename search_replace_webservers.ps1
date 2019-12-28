ec84ff0b428c4c78d1e4e7df58edcb718a0e4846
Brian Frost <brian@Birddogsw.local>
2019-12-28 14:23:05 -0600

    added a hacky change of spelling

--- search_replace_webservers.ps1
+++ search_replace_webservers.ps1
@@ -8,4 +8,12 @@ foreach ($file in $files){
   $replace = "enttestdb.birddogsw.local"
   $content = Get-Content $($file.FullName)
   $content.Replace($find, $replace) | Out-File $($file.FullName)  -Encoding UTF8
+
+  Write-Host $file.FullName
+  $bak = Join-Path $file.directory "birddog.config.bak2"
+  copy-item $file.FullName $bak
+  $find = "Office7"
+  $replace = "enttestdb.birddogsw.local"
+  $content = Get-Content $($file.FullName)
+  $content.Replace($find, $replace) | Out-File $($file.FullName)  -Encoding UTF8
 }
