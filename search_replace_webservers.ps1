6576d7292de31d4d4b9b8ccd5b9ecbaaf4e1d367
Brian Frost <brian@Birddogsw.local>
2019-12-28 14:15:42 -0600

    added a little search and replace script

---
+++ search_replace_webservers.ps1
@@ -0,0 +1,11 @@
+$files = Get-ChildItem -Filter BirdDog.config -Path "E:\Hosted Websites" -Recurse 
+
+foreach ($file in $files){
+  Write-Host $file.FullName
+  $bak = Join-Path $file.directory "birddog.config.bak"
+  copy-item $file.FullName $bak
+  $find = "office7"
+  $replace = "enttestdb.birddogsw.local"
+  $content = Get-Content $($file.FullName)
+  $content.Replace($find, $replace) | Out-File $($file.FullName)  -Encoding UTF8
+}
