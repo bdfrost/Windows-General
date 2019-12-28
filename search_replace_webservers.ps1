c686470ef842224bda215c55a1ff5dab58b624d8
Brian Frost <brian@Birddogsw.local>
2019-12-28 16:26:58 -0600

    still troubleshooting the find and replace

--- search_replace_webservers.ps1
+++ search_replace_webservers.ps1
@@ -1,19 +1,17 @@
 $files = Get-ChildItem -Filter BirdDog.config -Path "E:\Hosted Websites" -Recurse 
 
 foreach ($file in $files){
+ # Get-Item $file.FullName | Replace-FileString -Pattern 'office5' -Replacement 'replaceme' -Overwrite
   Write-Host $file.FullName
   $bak = Join-Path $file.directory "birddog.config.bak"
+  Write-Host $bak
   copy-item $file.FullName $bak
-  $find = "office7"
+#  $find = "office5"
   $replace = "enttestdb.birddogsw.local"
   $content = Get-Content $($file.FullName)
-  $content.Replace($find, $replace) | Out-File $($file.FullName)  -Encoding UTF8
+#  Write-Host $content
+  $content.Replace('office5', 'replaceme') | Out-File $file.FullName -Encoding UTF8
 
-  Write-Host $file.FullName
-  $bak = Join-Path $file.directory "birddog.config.bak2"
-  copy-item $file.FullName $bak
-  $find = "Office7"
-  $replace = "enttestdb.birddogsw.local"
-  $content = Get-Content $($file.FullName)
-  $content.Replace($find, $replace) | Out-File $($file.FullName)  -Encoding UTF8
+  $content.Replace("office5", $replace) | Out-File $($file.FullName) -Encoding UTF8
+  $content.Replace("Office5", $replace) | Out-File $($file.FullName) -Encoding UTF8
 }
